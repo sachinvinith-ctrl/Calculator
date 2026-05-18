a3fa3bd75a482bd1e4b9507fe43557876f7c3174
Sachin Vinith <233778418+sachinvinith-ctrl@users.noreply.github.com>
2026-05-18 21:06:41 +0530

    Add basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function addition(first, second) {
+
+return first + second;
+
+}
+
+function subtract(first, second) {
+
+return first - second;
+
+}
+
+function multiply(first, second) {
+
+return first * second;
+
+}
+
+
+function divide(first, second) {
+
+return first / second;
+
+}
