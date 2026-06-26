caba173a8913b7d3166e1d27e043f99a6335f8ed
Sachin Vinith <233778418+sachinvinith-ctrl@users.noreply.github.com>
2026-06-26 13:49:39 +0530

    Remove event listener to apply event delegation

--- script.js
+++ script.js
@@ -27,23 +27,20 @@ return first / second;
 
 
 let firstInput = [];
-let operator;
-let secondInput;
+let operator = "";
+let secondInput = [];
 
 
-const btn = document.querySelector(".button-box");
-btn.addEventListener("click", (event) =>  { firstInput.push(Number(event.target.closest("button").textContent.trim()))
 
 
-const displayBox = document.querySelector(".display-box");
-displayBox.style.display = "flex";
-displayBox.style.alignItems = "center";
-displayBox.textContent = firstInput.join("");
-displayBox.style.fontWeight = "bold";
-displayBox.style.fontSize = "150%";
 
 
-});
+
+
+
+
+
+
 
 
 
